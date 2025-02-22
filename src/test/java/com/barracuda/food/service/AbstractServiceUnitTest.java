119a9c1621907e93f33234826b8433b6634295f2
barracuda <dimitrijchannel@gmail.com>
2025-02-22 11:05:50 +0200

    Remove deprecated @MockBean and replace them with @MockitoBean

--- src/test/java/com/barracuda/food/service/AbstractServiceUnitTest.java
+++ src/test/java/com/barracuda/food/service/AbstractServiceUnitTest.java
@@ -4,17 +4,17 @@ import com.barracuda.food.AbstractTest;
 import com.barracuda.food.infrastructure.ServiceTest;
 import com.barracuda.food.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.MessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @ServiceTest
 public abstract class AbstractServiceUnitTest extends AbstractTest {
 
-    @MockBean
+    @MockitoBean
     protected UserRepository userRepositoryMock;
 
-    @MockBean
+    @MockitoBean
     protected PasswordEncoder passwordEncoder;
 
     @Autowired
