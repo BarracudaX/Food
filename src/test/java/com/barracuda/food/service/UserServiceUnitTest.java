7bc4d10fb4a0d814abd92c1ac97fe46cf01c3c64
barracuda <dimitrijchannel@gmail.com>
2025-02-22 11:05:29 +0200

    Add more test cases for user's name update functionality

--- src/test/java/com/barracuda/food/service/UserServiceUnitTest.java
+++ src/test/java/com/barracuda/food/service/UserServiceUnitTest.java
@@ -1,23 +1,28 @@
 package com.barracuda.food.service;
 
+import com.barracuda.food.dto.UpdateNameForm;
 import com.barracuda.food.dto.UserRegistrationForm;
 import com.barracuda.food.entity.User;
 import com.barracuda.food.infrastructure.ServiceTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ServiceTest
-public class UserServiceTest extends AbstractServiceUnitTest {
+public class UserServiceUnitTest extends AbstractServiceUnitTest {
 
     private final UserRegistrationForm registrationForm = UserRegistrationForm.builder().name("SOME_NAME").email("SOME@EMAIL.COM").password("SomePass123").repeatedPassword("SomePass123").build();
-    private final User user = new User(registrationForm.getName(),registrationForm.getEmail(),"ENCODED_PASSWORD");
+    private final User user = new User(1L,registrationForm.getName(),registrationForm.getEmail(),"ENCODED_PASSWORD");
     private final UserServiceImpl userService;
+    private final UpdateNameForm form = new UpdateNameForm("NewName",user.getId());
 
-    public UserServiceTest(UserServiceImpl userService) {
+    public UserServiceUnitTest(UserServiceImpl userService) {
         this.userService = userService;
     }
 
@@ -25,6 +30,7 @@ public class UserServiceTest extends AbstractServiceUnitTest {
     void setUp() {
         when(passwordEncoder.encode(registrationForm.getPassword())).thenReturn(user.getPassword());
         when(userRepositoryMock.save(any())).thenReturn(user);
+        when(userRepositoryMock.findById(user.getId())).thenReturn(Optional.of(user));
     }
 
     @Test
@@ -34,7 +40,22 @@ public class UserServiceTest extends AbstractServiceUnitTest {
 
         verify(passwordEncoder).encode(registrationForm.getPassword());
         verify(userRepositoryMock).save(savedUser.capture());
-        assertThat(savedUser.getValue()).usingRecursiveComparison().isEqualTo(user);
+        assertThat(savedUser.getValue()).usingRecursiveComparison().ignoringFields("id").isEqualTo(user);
     }
 
+    @Test
+    void shouldUpdateUserName() {
+        assertThat(user.getName()).isNotEqualTo(form.getName());
+
+        userService.changeUserName(form);
+
+        assertThat(user.getName()).isEqualTo(form.getName());
+    }
+
+    @Test
+    void shouldThrowEmptyResultDataAccessExceptionWhenTryingToUpdateNonExistingUser() {
+        when(userRepositoryMock.findById(form.getId())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> userService.changeUserName(form)).isInstanceOf(EmptyResultDataAccessException.class);
+    }
 }
