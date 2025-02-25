1b2464f0091bf7d590928a69a0b1645e27200419
barracuda <dimitrijchannel@gmail.com>
2025-02-25 15:41:42 +0200

    Add unit tests for UserEqualPasswordsValidator

---
+++ src/test/java/com/barracuda/food/validation/UserEqualPasswordsValidatorUnitTest.java
@@ -0,0 +1,51 @@
+package com.barracuda.food.validation;
+
+import com.barracuda.food.dto.UserCreateForm;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UserEqualPasswordsValidatorUnitTest {
+
+    private UserEqualPasswordsValidator validator = new UserEqualPasswordsValidator();
+
+    @Test
+    void shouldReturnTrueWhenBothPasswordAreNull() {
+        var form = new UserCreateForm("ANY",null,null,"ANY");
+
+        assertThat(validator.isValid(form,null)).withFailMessage("When both passwords are null, expect to get true.").isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseWhenOnlyOneOfThePasswordsIsNull() {
+        var form = new UserCreateForm("ANY",null,"notNull","ANY");
+        assertThat(validator.isValid(form,null))
+                .withFailMessage(() -> "When one password is null and the other isn't, expect to return false. Password = "+form.getPassword()+", repeated password = "+form.getRepeatedPassword())
+                .isFalse();
+
+        form.setPassword("notNull");
+        form.setRepeatedPassword(null);
+
+        assertThat(validator.isValid(form,null))
+                .withFailMessage(() -> "When one password is null and the other isn't, expect to return false. Password = "+form.getPassword()+", repeated password = "+form.getRepeatedPassword())
+                .isFalse();
+    }
+
+    @Test
+    void shouldReturnTrueWhenBothPasswordsAreEqual() {
+        var form = new UserCreateForm("ANY","EqualPasswords","EqualPasswords","ANY");
+
+        assertThat(validator.isValid(form,null))
+                .withFailMessage("When both passwords are equal, expect to get true. Password = "+form.getPassword()+", repeated password = "+form.getRepeatedPassword())
+                .isTrue();
+    }
+
+    @Test
+    void shouldReturnFalseWhenPasswordsDoNotMatch() {
+        var form = new UserCreateForm("ANY","Password","DiffPassword","ANY");
+
+        assertThat(validator.isValid(form,null))
+                .withFailMessage("When passwords do not match, expect to get false. Password = "+form.getPassword()+", repeated password = "+form.getRepeatedPassword())
+                .isFalse();
+    }
+}
